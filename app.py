ae74bcd5f121270b1e534136b09b4adc3f68a704
VaibhavUPratap <vaibhavupratap@gmail.com>
2025-10-11 21:23:30 +0530

    New

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_cors import CORS
 from database.db import init_db
 from routes.health_routes import health_bp
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -41,5 +42,7 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app = create_app()
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host="0.0.0.0", port=port)
+
